dc779e630b0abfbb81fccd0cba0a1fba1568c1f5
Su_Nes <ksuneps@gmail.com>
2025-04-10 12:05:55 +0300

    1st lecture commit

---
+++ KS_Legends/Assets/Scripts/PlayerMouseControl.cs
@@ -0,0 +1,69 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMouseControl : MonoBehaviour
+{
+    [SerializeField] private LayerMask layerMask;
+    [SerializeField] private float moveSpeed = 10f;
+    private CharacterController characterController;
+    private Animator animator;
+    private Camera playerCamera;
+    
+    private Vector3 targetPosition, lookPosition;
+    
+    
+    private void Start()
+    {
+        characterController = GetComponent<CharacterController>();
+        animator = GetComponent<Animator>();
+        playerCamera = Camera.main;
+        
+        targetPosition = transform.position;
+    }
+
+
+    private void Update()
+    {
+        float distToTarget = Vector3.Distance(transform.position, targetPosition);
+        if (distToTarget > 0.1f)
+        {
+            Vector3 targetDirection = (targetPosition - transform.position).normalized;
+            characterController.Move(targetDirection * (moveSpeed * Time.deltaTime));
+            
+            animator.SetBool("running", true);
+            
+            LookAtFlat();
+        }
+        else
+        {
+            animator.SetBool("running", false);
+        }
+        
+        if (Input.GetMouseButton(0))
+        {
+            targetPosition = GetTargetPosition();
+        }
+    }
+
+    private void LookAtFlat()
+    {
+        lookPosition = targetPosition;
+        lookPosition.y = transform.position.y;
+        transform.LookAt(lookPosition);
+    }
+
+    private Vector3 GetTargetPosition()
+    {
+        RaycastHit hit;
+        Ray ray = playerCamera.ScreenPointToRay(Input.mousePosition);
+
+        if (Physics.Raycast(ray, out hit, 500f, layerMask, QueryTriggerInteraction.Collide))
+        {
+            return hit.point;
+        }
+
+        return transform.position;
+    }
+}
